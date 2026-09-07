abd0339e8c194c74dcc07bfe43ba672316ee00be
Juan Manuel <garciacanete85@gmail.com>
2026-09-07 22:17:27 +0200

    Add inline field editing checklist progress and field filters

--- connector.js
+++ connector.js
@@ -1,53 +1,32 @@
 var BASE_URL = 'https://garciacanete.github.io/custom-fields-trello-free/';
+var POWERUP_VERSION = 'v8';
 var GRAY_ICON = BASE_URL + 'icon.svg';
 var WHITE_ICON = BASE_URL + 'icon.svg';
 
 var getChecklistProgress = function(t) {
-  return t.card('id').then(function(card) {
-    var restApi = t.getRestApi();
+  return t.card('checklists').then(function(card) {
+    var total = 0;
+    var done = 0;
 
-    return restApi.isAuthorized()
-      .then(function(isAuthorized) {
-        if (!isAuthorized) {
-          return {
-            text: '0/0 (0%)',
-            color: 'red'
-          };
+    (card.checklists || []).forEach(function(cl) {
+      (cl.checkItems || []).forEach(function(item) {
+        total++;
+        if (item.state === 'complete') {
+          done++;
         }
-
-        return restApi.get('/1/cards/' + card.id + '/checklists')
-          .then(function(checklists) {
-            var total = 0;
-            var done = 0;
-
-            (checklists || []).forEach(function(cl) {
-              (cl.checkItems || []).forEach(function(item) {
-                total++;
-                if (item.state === 'complete') {
-                  done++;
-                }
-              });
-            });
-
-            var pct = total > 0 ? Math.round((done / total) * 100) : 0;
-            var color = 'red';
-
-            if (pct >= 100 && total > 0) color = 'green';
-            else if (pct >= 50) color = 'yellow';
-
-            return {
-              text: done + '/' + total + ' (' + pct + '%)',
-              color: color
-            };
-          });
-      })
-      .catch(function(err) {
-        console.error('Error reading checklist progress', err);
-        return {
-          text: '0/0 (0%)',
-          color: 'red'
-        };
       });
+    });
+
+    var pct = total > 0 ? Math.round((done / total) * 100) : 0;
+    var color = 'red';
+
+    if (pct >= 100 && total > 0) color = 'green';
+    else if (pct >= 50) color = 'yellow';
+
+    return {
+      text: done + '/' + total + ' (' + pct + '%)',
+      color: color
+    };
   });
 };
 
@@ -57,7 +36,7 @@ TrelloPowerUp.initialize({
       return [{
         text: progress.text,
         color: progress.color,
-        icon: progress.color === 'white' ? WHITE_ICON : GRAY_ICON
+        icon: GRAY_ICON
       }];
     });
   },
@@ -76,8 +55,8 @@ TrelloPowerUp.initialize({
       icon: GRAY_ICON,
       content: {
         type: 'iframe',
-        url: t.signUrl(BASE_URL + 'card-back.html'),
-        height: 200
+        url: t.signUrl(BASE_URL + 'card-back.html?' + POWERUP_VERSION),
+        height: 420
       }
     };
   },
@@ -88,30 +67,43 @@ TrelloPowerUp.initialize({
       callback: function(t) {
         return t.popup({
           title: 'Editar Campos',
-          url: t.signUrl(BASE_URL + 'card-fields.html'),
-          height: 400
+          url: t.signUrl(BASE_URL + 'card-fields.html?' + POWERUP_VERSION),
+          height: 500
         });
       }
     }];
   },
   'board-buttons': function(t, options) {
-    return [{
-      icon: WHITE_ICON,
-      text: 'Gestionar Campos',
-      callback: function(t) {
-        return t.popup({
-          title: 'Configuración de Campos',
-          url: t.signUrl(BASE_URL + 'settings.html'),
-          height: 600
-        });
+    return [
+      {
+        icon: WHITE_ICON,
+        text: 'Gestionar Campos',
+        callback: function(t) {
+          return t.popup({
+            title: 'Configuración de Campos',
+            url: t.signUrl(BASE_URL + 'settings.html?' + POWERUP_VERSION),
+            height: 650
+          });
+        }
+      },
+      {
+        icon: WHITE_ICON,
+        text: 'Filtrar Campos',
+        callback: function(t) {
+          return t.popup({
+            title: 'Filtrar por Campos',
+            url: t.signUrl(BASE_URL + 'filters.html?' + POWERUP_VERSION),
+            height: 650
+          });
+        }
       }
-    }];
+    ];
   },
   'show-settings': function(t, options) {
     return t.popup({
       title: 'Configuración del Power-Up',
-      url: t.signUrl(BASE_URL + 'settings.html'),
-      height: 600
+      url: t.signUrl(BASE_URL + 'settings.html?' + POWERUP_VERSION),
+      height: 650
     });
   },
   'card-move-restrictions': function(t, options) {
